8b079677948b49f77af4da882d2b4d304ed9ca53
oluiwadamilola <damilola.ogunyemi@identikosolutions.com>
2023-03-22 12:56:58 +0100

    Added http factory for create and update application test

--- CapitalPlacementApiTest/UnitTestFactory.cs
+++ CapitalPlacementApiTest/UnitTestFactory.cs
@@ -14,19 +14,28 @@ namespace CapitalPlacementApiTest
 {
     public class UnitTestFactory
     {
+
         public static HttpRequest GetHttpRequest()
         {
             var context = new DefaultHttpContext();
             return context.Request;
         }
 
-        public static HttpRequest HttpRequest()
+        public static HttpRequest HttpRequest(bool isCreateApplication)
         {
+            object? json;
             var httpContext = new DefaultHttpContext();
+            
+            if (isCreateApplication)
+            {
+                 json = JsonConvert.DeserializeObject("{ 'Category': 'Job test',  'Title ': 'Test title 2 ', 'Summary ': 'Test Summary ',  'Description ': 'Test description ', 'Skils ': 3, 'Benefits ': 'Test benefits ', 'Criteria ': 'Test criteria ', 'Type ': 1, 'Start ': '2023-03-16 ',        'Open ': '2023-03-16 ', 'Close ': '2023-04-16 ', 'DurationInMonths ':6, 'Location ':2, 'MinimumQualification ':3, 'MaxNumOfApplication ':1, 'Image ': '' , 'Firstname ': 'Test firstname ', 'LastName ':  'Test last name ', 'Email ': 'Test@email.com ', 'Phone ': '12345678 ', 'Nationality ':  'Nigeria ', 'CurrentResidence ': 'Lagos ', 'IDNumber ': '887766578 ', 'DateOfBirth ': '2000-05-16 ', 'Gender ':2, 'PersonalInformationQuestions ':[ { 'QuestionType ': 1, 'Question ':  'test question here ', 'MaxChoiceAllowed ': 2 }, { 'QuestionType ': 2, 'Question ':  'test question2  here ', 'MaxChoiceAllowed ': 1 }   ], 'Education ': 'Computer science ', 'Experience ':  '2 ', 'Resume ':  'test question2  here ', 'ProfileQuestions ':[          { 'QuestionType ': 1,            'Question ':  'test question here ',            'MaxChoiceAllowed ': 2 }, { 'QuestionType ': 2,            'Question ':  'test question2  here ',            'MaxChoiceAllowed ': 1           } ],          'Name ':  'Test name ',         'VideoType ':  'Test name ',         'Placement ':  'Test name ',         'DisplayStageToCandidate ': true,         'VideoInterview ': [ { 'InterviewQuestion ':  'test intervies question ', 'DurationInSec ': 45, 'DeadlineInDays ': 5   }, {  'InterviewQuestion ':  'test intervies question ',    'DurationInSec ': 45,  'DeadlineInDays ': 5 } ]   }");
+            }
+            else
+            {
+                json = JsonConvert.DeserializeObject("{ 'Category': 'Updated Job test ',  'Title ': 'Updated Test title 2 ', 'Summary ': ' Updated Test Summary ',  'Description ': ' Updated Test description ', 'Skils ': 3, 'Benefits ': 'Updated Test benefits ', 'Criteria ': 'Updated Test criteria ', 'Type ': 1, 'Start ': '2023-03-16 ','Open ': '2023-03-16 ','Close ': '2023-04-16 ','DurationInMonths ':6,'Location ':2, 'MinimumQualification ':3, 'MaxNumOfApplication ':1, 'Image ': '' , 'Firstname ': 'Updated Test firstname ', 'LastName ':  'Updated Test last name ', 'Email ': 'UpdatedTest@email.com ', 'Phone ': '12345678 ', 'Nationality ':  'Nigeria ', 'CurrentResidence ': 'Lagos ', 'IDNumber ': '887766578 ', 'DateOfBirth ': '2000-05-16 ', 'Gender ':2, 'PersonalInformationQuestions ':[ {'QuestionType ': 1,'Question ':  'test question here ', 'MaxChoiceAllowed ': 2 }, { 'QuestionType ': 2, 'Question ':  'test question2  here ', 'MaxChoiceAllowed ': 1 }   ], 'Education ': 'Computer science ', 'Experience ':  '2 ', 'Resume ':  'test question2  here ',          'ProfileQuestions ':[          {            'QuestionType ': 1, 'Question ':  'test question here ',            'MaxChoiceAllowed ': 2           },            {            'QuestionType ': 2,            'Question ':  'test question2  here ',            'MaxChoiceAllowed ': 1           } ],          'Name ':  'Test name ',         'VideoType ':  'Test name ',         'Placement ':  'Test name ', 'DisplayStageToCandidate ': true, 'VideoInterview ': [ { 'InterviewQuestion ':  'test intervies question ', 'DurationInSec ': 45, 'DeadlineInDays ': 5   }, {  'InterviewQuestion ':  'test intervies question ', 'DurationInSec ': 45,  'DeadlineInDays ': 5 } ]   }");
 
-            var json = JsonConvert.DeserializeObject("{ 'Category': 'Remote Job',  'Title ': 'Test title 2 ', 'Summary ': 'Test Summary ',  'Description ': 'Test description ', 'Skils ': 3, 'Benefits ': 'Test benefits ', 'Criteria ': 'Test criteria ', 'Type ': 1, 'Start ': '2023-03-16 ',        'Open ': '2023-03-16 ',        'Close ': '2023-04-16 ',        'DurationInMonths ':6,        'Location ':2, 'MinimumQualification ':3, 'MaxNumOfApplication ':1, 'Image ': '' , 'Firstname ': 'Test firstname ', 'LastName ':  'Test last name ', 'Email ': 'Test@email.com ', 'Phone ': '12345678 ', 'Nationality ':  'Nigeria ', 'CurrentResidence ': 'Lagos ', 'IDNumber ': '887766578 ', 'DateOfBirth ': '2000-05-16 ', 'Gender ':2,          'PersonalInformationQuestions ':[             {              'QuestionType ': 1,              'Question ':  'test question here ',              'MaxChoiceAllowed ': 2             },               {              'QuestionType ': 2,              'Question ':  'test question2  here ',               'MaxChoiceAllowed ': 1             }   ],          'Education ': 'Computer science ',        'Experience ':  '2 ',        'Resume ':  'test question2  here ',          'ProfileQuestions ':[          {            'QuestionType ': 1,            'Question ':  'test question here ',            'MaxChoiceAllowed ': 2           },            {            'QuestionType ': 2,            'Question ':  'test question2  here ',            'MaxChoiceAllowed ': 1           } ],          'Name ':  'Test name ',         'VideoType ':  'Test name ',         'Placement ':  'Test name ',         'DisplayStageToCandidate ': true,         'VideoInterview ': [            { 'InterviewQuestion ':  'test intervies question ', 'DurationInSec ': 45, 'DeadlineInDays ': 5   }, {  'InterviewQuestion ':  'test intervies question ',    'DurationInSec ': 45,  'DeadlineInDays ': 5         } ]   }");
-           
-        
+            }
+
             var stream = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject( json )));
             httpContext.Request.Body = stream;
             httpContext.Request.ContentLength = stream.Length;
